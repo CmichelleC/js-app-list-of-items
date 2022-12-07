30d93f0da0466eaedd305a94cc3c0abbf55c2673
CmichelleC <chloe.comeaux@gmail.com>
2022-12-07 14:35:51 -0600

    Changed first addition to correct form and added other two pokemon to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 let pokemonList = [
-    'Bulbasaur',
-    {height: '7'},
-    ['grass', 'poison']
-  ]
-  
+  {name = 'bulbasaur', height = '7', ['grass', 'poison'],}:
+  {name = 'Zubat', height = '8', ['poison', 'flying'], }:
+  {name = 'Beedrill', height = '10', ['bug', 'poison'],}:
+]  
