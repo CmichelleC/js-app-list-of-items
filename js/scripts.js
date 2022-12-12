b76488571c1bca0ddf09ebda86ade3da74c3a14e
CmichelleC <chloe.comeaux@gmail.com>
2022-12-12 15:44:46 -0600

    Corrected small syntax errors and added a key

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
-  {name = 'bulbasaur', height = '7', ['grass', 'poison'],}:
-  {name = 'Zubat', height = '8', ['poison', 'flying'], }:
-  {name = 'Beedrill', height = '10', ['bug', 'poison'],}:
-]  
+  {name: 'bulbasaur', height = 7, ['grass', 'poison']},
+  {name: 'Zubat', height = 8, ['poison', 'flying']},
+  {name: 'Beedrill', height = 10, ['bug', 'poison']},
+];
+console.log(pokemonList[0]);
