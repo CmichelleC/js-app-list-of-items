9000c8e49d60fc6e6b35e08976b405579ceb10a0
CmichelleC <chloe.comeaux@gmail.com>
2023-01-24 11:53:51 -0600

    Changed equal sign after height to colon and added a key to last part of array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 let pokemonList = [
-  {name: 'bulbasaur', height = 7, ['grass', 'poison']},
-  {name: 'Zubat', height = 8, ['poison', 'flying']},
-  {name: 'Beedrill', height = 10, ['bug', 'poison']},
+  {name: 'bulbasaur', height: 7, types: ['grass', 'poison']},
+  {name: 'Zubat', height: 8, types: ['poison', 'flying']},
+  {name: 'Beedrill', height: 10, types: ['bug', 'poison']},
 ];
-console.log(pokemonList[0]);
