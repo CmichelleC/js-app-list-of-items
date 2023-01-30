1771ef25fd1a749579609e9bbe97e60950f5bf06
CmichelleC <chloe.comeaux@gmail.com>
2023-01-30 15:45:41 -0600

    Created for loop with conditionals and message for largest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 let pokemonList = [
-  {name: 'bulbasaur', height: 7, types: ['grass', 'poison']},
+  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
   {name: 'Zubat', height: 8, types: ['poison', 'flying']},
   {name: 'Beedrill', height: 10, types: ['bug', 'poison']},
 ];
+for (let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height <=7){
+    document.write(pokemonList[i].name + " - Height: 7" + "<br>") 
+  }else if (pokemonList[i].height >7 && pokemonList[i].height<9){
+    document.write(pokemonList[i].name + " - Height: 8" + "<br>")
+  }else {
+    document.write(pokemonList[i].name + " - Height: 10" + " (Wow, that's big!)" + "<br>")
+  }
+}
